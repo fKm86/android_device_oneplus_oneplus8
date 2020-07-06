272a584ba5d87c93a282001e590ef9db99b3ac23
fKm <fmetzner86@gmail.com>
2020-07-06 20:17:37 +0200

    oneplus8: Import bootctrl and gpt-utils

--- device.mk
+++ device.mk
@@ -27,10 +27,13 @@ PRODUCT_PACKAGES += \
     libbluetooth_qti
 
 # Boot
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service \
+    android.hardware.boot@1.0-impl.recovery \
+    librecovery_updater_msm \
     bootctrl.kona \
-    libcutils \
-    libz
+    bootctrl.kona.recovery
 
 # Common
 $(call inherit-product, device/oneplus/common/common.mk)
