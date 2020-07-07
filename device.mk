e362df4db4c13eca2a56d3f3b677198dac43521f
fKm <fmetzner86@gmail.com>
2020-07-07 12:57:41 +0200

    oneplus8: add back librecovery_updater_msm

--- device.mk
+++ device.mk
@@ -34,6 +34,7 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service \
     android.hardware.boot@1.0-impl.recovery \
+    librecovery_updater_msm \
     bootctrl.kona \
     bootctrl.kona.recovery
 
